a849b95d976cf8cf7c92efad6fb13fc0421dfd64
hashylog <108955419+hashylog@users.noreply.github.com>
2025-10-31 19:24:52 -0300

    main.lua

--- .config/nvim/lua/plugins/main.lua
+++ .config/nvim/lua/plugins/main.lua
@@ -13,9 +13,24 @@ return
   {
     "hashylog/micro-motion.nvim",
     config = function()
-      require("micro-motion").setup()
+      local micro_motion = require("micro-motion")
+      micro_motion.setup()
+
+      vim.keymap.set('n', '<C-S-Right>', function()
+        vim.cmd('normal! v')
+        micro_motion.word_right()
+      end, { noremap = true, silent = true, desc = 'Select word right (micro)' })
+
+      vim.keymap.set('n', '<C-S-Left>', function()
+        vim.cmd('normal! v')
+        micro_motion.word_left()
+      end, { noremap = true, silent = true, desc = 'Select word left (micro)' })
+
+      vim.keymap.set('v', '<C-S-Right>', micro_motion.word_right, { noremap = true, silent = true, desc = 'Extend selection right (micro)' })
+      vim.keymap.set('v', '<C-S-Left>', micro_motion.word_left, { noremap = true, silent = true, desc = 'Extend selection left (micro)' })
     end
   },
+
   {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
