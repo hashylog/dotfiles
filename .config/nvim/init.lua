629d1b7a4f5caa97f9ad17851f223cff7f3789a9
hashylog <108955419+hashylog@users.noreply.github.com>
2025-10-31 19:09:59 -0300

    init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,62 @@
+-- Lazy
+require("config.lazy")
+-- NVide
+require('nvide')
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true;
+
+-- Make line numbers default
+vim.o.number = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+vim.opt.backup = false
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = false
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 0
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+vim.o.confirm = true
+
+-- Convert tabs to spaces
+vim.opt.expandtab = true
+
+-- Colorscheme
+vim.cmd("colorscheme kanagawa-dragon");
