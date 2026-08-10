a0148261ba383b6de307430649e9a125b2cdf5fd
hashylog <108955419+hashylog@users.noreply.github.com>
2026-08-09 23:30:28 -0300

    .

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,31 +8,37 @@ let
   kde-blur = pkgs.kdePackages.callPackage ./pkgs/kde-blur/nix/package.nix { };
 in
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix ];
 
+  # Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Bootloader.
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # Hostname
+  networking.hostName = "nixos";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-  # Enable networking
+  # Networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # Time Zone
   time.timeZone = "America/Sao_Paulo";
 
-  # Select internationalisation properties.
+  # Keymap
+  services.xserver.xkb = {
+    layout = "br";
+    variant = "";
+  };
+
+  # Console Keymap
+  console.keyMap = "br-abnt2";
+
+  # Locale settings
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -47,27 +53,7 @@ in
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "br";
-    variant = "";
-  };
-
-  # Configure console keymap
-  console.keyMap = "br-abnt2";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
+  # Pipewire
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -75,80 +61,67 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    jack.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+# --- --- ---
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # hashylog
   users.users."hashylog" = {
     isNormalUser = true;
-    shell = pkgs.zsh;
+    #shell = pkgs.zsh;
     description = "hashylog";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
-    ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
+  # Disable Sudo Password
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  # KDE Plasma
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # System Packages
   environment.systemPackages = with pkgs; [
     wget
     git
     git-credential-oauth
     vscodium
     kde-blur
-    neovim
     emacs-nox
+    neovim
+    zip
+    unzip
+    rar
+    unrar
+    firefox
+    zsh
   ];
 
-  # fonts
+  # Flatpak
+  services.flatpak.enable = true;
+
+  # Fonts
   fonts.packages = with pkgs; [
     jetbrains-mono
     nerd-fonts.jetbrains-mono
   ];
 
-  # zsh
-  programs.zsh.enable = true;
-
-  # disable sudo password
-  security.sudo = {
+  # Docker
+  virtualisation.docker = {
     enable = true;
-    wheelNeedsPassword = false;
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # Run generic linux dynamically linked executables
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      # ...
+    ];
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
